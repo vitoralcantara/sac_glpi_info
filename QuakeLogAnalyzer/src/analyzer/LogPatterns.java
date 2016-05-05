7a32695da539975f1919d73c11d090e74460807f
Vítor Alcântara de Almeida <vitoralcantara@ppgsc.ufrn.br>
2016-05-05 01:43:55 -0300

    Finished implementation of method parseReader

---
+++ QuakeLogAnalyzer/src/analyzer/LogPatterns.java
@@ -0,0 +1,6 @@
+package analyzer;
+
+public class LogPatterns {
+	public static final String KILL = "(\\b\\d?\\d?:\\d\\d) Kill: (\\d+) (\\d+) (\\d+): (<world>|\\w+[\\w ]*) killed (\\w+[\\w ]*) by (\\w+)\\b";
+
+}
--- QuakeLogAnalyzer/src/analyzer/QuakeLogAnalyzer.java
+++ QuakeLogAnalyzer/src/analyzer/QuakeLogAnalyzer.java
@@ -14,7 +14,7 @@ import java.util.regex.Pattern;
 public class QuakeLogAnalyzer {
 
 	public static QuakeLogAnalyzedResults parse(String filename)
-			throws FileNotFoundException {
+			throws IOException {
 		BufferedReader reader = openFile(filename);
 		return parseReader(reader);
 	}
@@ -23,21 +23,42 @@ public class QuakeLogAnalyzer {
 			throws IOException {
 		Map<String, Long> kills = new HashMap<String, Long>();
 		Map<String, Long> killByMeans = new HashMap<String, Long>();
-		long totalKills = 0;
+		long totalKills = 0; // assumed this count is same value as the sum of
+								// the kill of all players
 
 		while (reader.ready()) {
 			String line = reader.readLine();
-			Pattern pattern = Pattern
-					.compile("\\b\\d?\\d?:\\d\\d Kill: \\d+ \\d+ \\d+: (<world>|\\w+[\\w ]*) killed (\\w+[\\w ]*) by \\w+\\b");
+			Pattern pattern = Pattern.compile(LogPatterns.KILL);
 			Matcher matcher = pattern.matcher(line);
 			if (!matcher.matches()) {
 				continue; // the line is not a death log.
 			} else {
-				
+				String killer = matcher.group(0);
+				String killed = matcher.group(0);
+				String meanKill = matcher.group(0);
+				if (killer.equals("<world>")) {
+					--totalKills;
+					// decrease number of kills of killed player
+					updateHashMap(kills, killed, new Long(-1));
+				} else {
+					++totalKills;
+					// increase number of kills of killer player
+					updateHashMap(kills, killer, new Long(1));
 
+					// update kill mean count
+					updateHashMap(killByMeans, meanKill, new Long(1));
+				}
 			}
 		}
 
+		QuakeLogAnalyzedResults results = new QuakeLogAnalyzedResults(
+				totalKills, kills, killByMeans);
+		return results;
+	}
+
+	private static void updateHashMap(Map<String, Long> map, String obj, Long count) {
+		long value = map.getOrDefault(obj, new Long(0));
+		map.put(obj, value + count);
 	}
 
 	private static BufferedReader openFile(String filename)
